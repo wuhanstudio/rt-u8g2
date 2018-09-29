45ad5b59a9fd679f56bf8f69710426c97d8ad594
Wu Han <wuhanstudio@hust.edu.cn>
2018-09-29 22:40:50 +0800

    Software I2C merged into rt-thread, not required here

--- port/u8g_port.c
+++ port/u8g_port.c
@@ -6,18 +6,18 @@ static struct rt_i2c_bus_device *i2c_bus = RT_NULL;
 
 uint8_t u8x8_rt_gpio_and_delay(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr)
 {
-  switch(msg)
-  {
+	switch(msg)
+	{
 	case U8X8_MSG_DELAY_NANO:			// delay arg_int * 1 nano second
-		__asm__ volatile("nop"); 
-		break;    
+		__asm__ volatile("nop");
+		break;
 	case U8X8_MSG_DELAY_100NANO:		// delay arg_int * 100 nano seconds
-		__asm__ volatile("nop"); 
+		__asm__ volatile("nop");
 		break;
 	case U8X8_MSG_DELAY_10MICRO:		// delay arg_int * 10 micro seconds
 		for (uint16_t n = 0; n < 320; n++)
 		{
-			__asm__ volatile("nop"); 
+			__asm__ volatile("nop");
 		}
 		break;
 	case U8X8_MSG_DELAY_MILLI:			// delay arg_int * 1 milli second
@@ -25,23 +25,23 @@ uint8_t u8x8_rt_gpio_and_delay(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void 
 		break;
 	default:
 		return 0;
-  }
-  return 1;
+	}
+	return 1;
 }
 
 uint8_t u8x8_byte_rt_hw_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr)
 {
 	/* u8g2/u8x8 will never send more than 32 bytes between START_TRANSFER and END_TRANSFER */
 	struct rt_i2c_msg msgs;
-	static uint8_t buffer[32];		
+	static uint8_t buffer[32];
 	static uint8_t buf_idx;
 	uint8_t *data;
 	
-	rt_uint8_t t = 0;	
+	rt_uint8_t t = 0;
 	switch(msg)
 	{
 		case U8X8_MSG_BYTE_SEND:
-			data = (uint8_t *)arg_ptr;      
+			data = (uint8_t *)arg_ptr;
 			while( arg_int > 0 )
 			{
 				buffer[buf_idx++] = *data;
@@ -60,8 +60,6 @@ uint8_t u8x8_byte_rt_hw_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *ar
 		case U8X8_MSG_BYTE_SET_DC:
 			break;
 		case U8X8_MSG_BYTE_START_TRANSFER:
-			// Set I2C Address
-			//setI2CSlave(i2c_device, u8x8_GetI2CAddress(u8x8)>>1);
 			buf_idx = 0;
 			break;
 		case U8X8_MSG_BYTE_END_TRANSFER:
@@ -71,10 +69,10 @@ uint8_t u8x8_byte_rt_hw_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *ar
 				return 0;
 			}
 			// I2C Data Transfer
-			msgs.addr = u8x8_GetI2CAddress(u8x8)>>1;
+			msgs.addr  = u8x8_GetI2CAddress(u8x8)>>1;
 			msgs.flags = RT_I2C_WR;
-			msgs.buf = buffer;
-			msgs.len = buf_idx;
+			msgs.buf   = buffer;
+			msgs.len   = buf_idx;
 			while(rt_i2c_transfer(i2c_bus, &msgs, 1) != 1 && t < MAX_RETRY)
 			{
 				t++;
